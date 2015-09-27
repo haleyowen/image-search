b5dc7e6d61c5c9b79e82a3eab085e5a80f846566
Brady Zhou <brady.zhou@utexas.edu>
2015-09-27 05:31:08 -0400

    switched to ajax, tags almost working

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from flask import Flask, render_template, request, jsonify
 import json
 import os
 import uuid
-from ProcessImage import process_image
+import requests
 
 from elasticsearch import Elasticsearch
 
 application = Flask(__name__, static_folder="static")
 application.config['DEBUG'] = True
+
+current_image_id = -1
+name = -1
+full_name = "UNDEFINED"
+access_token = None
 
 
 @application.route("/")
@@ -18,37 +23,36 @@ def api_root():
 
 @application.route("/upload_file", methods=["POST"])
 def api_upload():
+    global name, full_name
     if request.method != 'POST':
         return "no"
 
     upload = request.files['file']
     image_id = str(uuid.uuid4())
     name = image_id+"."+upload.filename.split(".")[-1]
+    full_name = "http://45.55.45.85/static/uploads/"+str(name)
 
-    file_path = os.path.join(os.getcwd(), "static/uploads/")+name
+    file_path = os.path.join(os.getcwd(), "static/uploads/")+str(name)
 
     upload.save(file_path)
 
-    print(file_path)
-    print(name)
-
     # tags = process_image(["http://45.55.45.85/static/uploads/"+name])
 
-    doc = {
-        "image_name": image_id,
-        "tags": {}
-    }
+    # doc = {
+    #     "image_name": image_id,
+    # }
+    global current_image_id
+    current_image_id = image_id
 
-    es = Elasticsearch()
-    es.index(index="image-search", doc_type="image", id=image_id,
-             body=json.dumps(doc))
+    # es = Elasticsearch()
+    # es.index(index="image-search", doc_type="image", id=image_id,
+    #         body=json.dumps(doc))
 
     return jsonify({"message": "success"})
 
 
-@application.route("/image/search", methods=["POST"])
-def api_search():
-    # search_terms = request.POST['search-terms']
+@application.route("/post_tags", methods=["POST"])
+def api_post_tags():
     # query = {
     #     "query": {
     #         "match": {
@@ -56,20 +60,60 @@ def api_search():
     #         }
     #     }
     # }
+
+    global full_name, current_image_id
+
+    tags = request.json
+
+    print(tags)
+
+    doc = {
+        "image_name": current_image_id,
+        "tags_hits": tags
+    }
+
+    es = Elasticsearch()
+    es.index(index="image-search", doc_type="image", id=current_image_id,
+             body=doc)
+
+    print('made it')
     query_ids = {
         "query": {
             "match_all": {}
-        },
-        "fields": ["_id"]
+        }
     }
-    es = Elasticsearch()
-    results = es.search(index="image-search", body=json.dumps(query_ids))
+
+    results = es.search(index="image-search", body=query_ids)
+
     print(results)
     return json.dumps(results)
 
 
 def api_get_files():
     return 0
+
+
+@application.route("/post_url", methods=["POST"])
+def api_post_url():
+    global access_token
+
+    if not access_token:
+        r = requests.post("https://api.clarifai.com/v1/token/?grant_type=" +
+                          "client_credentials&client_id=KuQ2EWXhdosMQyktS" +
+                          "I1tw6Z3be7c677oA11p1g9o&client_secret=Kt84XYqK" +
+                          "7nVnx3f05_B8pe2biv9bBlGZAHjFvEwF")
+        access_token = r.json()['access_token']
+
+    return str('https://api.clarifai.com/v1/tag/?url=' +
+               full_name+'&access_token='+access_token)
+
+
+def extract_tags_dict(obj):
+    print(obj['results'][0]['result'])
+    data = obj['results'][0]['result']['tag']
+    tags = data['classes']
+    probs = data['probs']
+    return dict(zip(tags, probs))
 
 if __name__ == "__main__":
     application.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,25 @@
 $(function(){
+  var link = '';
   $('#search').hide();
 
   $('#button').click(function() {
-    $.post('image/search', function(data, status) {
-      console.log(data);
+    $.post('post_url', function(link, status){
+      console.log(link);
+
+      $.get(link, function(data){
+        tags = data['results'][0]['result']['tag']['classes'];
+        console.log(tags);
+
+        $.ajax('post_tags', {
+          data : JSON.stringify(tags),
+          contentType : 'application/json',
+          type : 'POST',
+          success: function(data) {
+            console.log(data);
+          }
+        })
+      })
     });
   });
+
 });
